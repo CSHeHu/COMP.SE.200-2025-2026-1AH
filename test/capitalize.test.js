39c21518e86504a28ac8fba4cfc7f7e5efd07a2e
AK <ari.kukkaro@tuni.fi>
2025-11-12 14:06:07 +0200

    test: eq utility AI generated unit tests added.

--- test/capitalize.test.js
+++ test/capitalize.test.js
@@ -5,17 +5,25 @@ describe("Testing capitalize utility", () => {
     it("Some input chars uppercase, Should return only first char uppercase.", () => {
         
         expect(capitalize("cOfFeE")).to.equal("Coffee");
+        expect(capitalize("CoFfEe")).to.equal("Coffee");
     });
     it("Empty string should, return empty string", () => {
         
         expect(capitalize("")).to.equal("");
     });
-     it("Invalid input, should throw error", () => {
+    it("Number should return number?", () => {
+        // Number to capitalize should return what?
+        expect(capitalize(1.6)).to.equal(1.6);
+    });
+     it("Invalid input, should throw error or return itself", () => {
         // when capitalizing null, returns Null
         expect(capitalize(null)).to.equal("null");
-        
-        
+        expect(capitalize(undefined)).to.equal(undefined);
     });
-    
+    it("Invalid input, should throw error or return itself", () => {
+        // when capitalizing undefined, returns Undefined, should it?
+        expect(capitalize(undefined)).to.equal(undefined);
+    });
+
 })
 
