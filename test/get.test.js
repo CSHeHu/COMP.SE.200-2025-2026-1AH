5a6fc96479171725f3a6d11e7b6c1186d0af79ee
AK <ari.kukkaro@tuni.fi>
2025-11-12 11:48:20 +0200

    test: Added get utility unit test.

---
+++ test/get.test.js
@@ -0,0 +1,10 @@
+import { expect } from "chai";
+import get from "../src/get.js";
+
+describe("Testing get utility: gets object value from path, if path undefined returns default value", () => {
+    it("Get object from path", () => {
+        const object = { 'a': [{ 'b': { 'c': 10 } }] }
+        expect(get(object, 'a[0].b.c')).to.equal(10);
+    });
+    
+})
