7b91c5ce64d35d5b4af78d582bf58f33783fa1b1
AK <ari.kukkaro@tuni.fi>
2025-11-12 11:37:46 +0200

    test: Updated difference utility unit tests.

--- test/difference.test.js
+++ test/difference.test.js
@@ -1,16 +1,22 @@
-import { AssertionError, expect } from "chai";
+import { expect } from "chai";
 import difference from "../src/difference.js";
 
-describe("Testing difference utility: compares two arrays. Returns", () => {
-    it("Should return array", () => {
+describe("Testing difference utility: compares two arrays. Returns array", () => {
+    it("Comparing longer array to shorter", () => {
         
-        expect(difference([],[])).to.be.an('array');
+        expect(difference([2,1,5,6],[2,3])).to.eql([1,5,6]);
     });
-    it("Comparing same size arrays", () => {
+    it("Comparing shorter array to longer", () => {
         
-        expect(difference([2,1],[2,3])).to.eql([1]);
+        expect(difference([2,1,"a"],[2,3,4,5])).to.eql([1,"a"]);
     });
-    
-    
+    it("Comparing arrays with unusual values such as null or Nan", () => {
+        
+        expect(difference([null,NaN,undefined],[NaN])).to.eql([null,undefined]);
+    });
+    it("Comparing non-array to array. Returns empty array", () => {
+        
+        expect(difference(null,[2])).to.eql([]);
+    });
     
 })
